1add728fa8230c79164fb09e10e00b5a3dc9ffa6
camillapalombi <camilla.palombi14@gmail.com>
2022-04-11 17:16:00 +0200

    Footer

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <!--HEADER -->
@@ -126,5 +127,31 @@
     
 
     </main>
+
+    <!--FOOTER -->
+    <footer>
+
+        <div class="footer-container">
+            <ul class="footer-links">
+                <li><a href="#!">Google</a></li>
+                <li><a href="#!">Tutto su Google</a></li>
+                <li><a href="#!">Privacy</a></li>
+                <li><a href="#!">Termini</a></li>
+            </ul>
+            
+            <div class="footer-right">
+                <i class="fa-solid fa-globe"></i>
+                <select id="languages">
+                    <option value="it">Italiano</option>
+                    <option value="en">English</option>
+                    <option value="es">Espanol</option>
+                    <option value="dt">Deutsch</option>
+                    <option value="fr">Francais</option>
+                </select>
+            </div>
+        </div>
+
+    </footer>
+
 </body>
 </html>
